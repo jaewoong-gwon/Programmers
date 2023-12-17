e3aa7eab40b0a7afba61e68d06c16dcce5f86b8b
Jaeung <75228991+jaewoong-gwon@users.noreply.github.com>
2023-12-18 02:44:05 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 172 ms, Memory: 16004 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.java
@@ -1,34 +1,33 @@
 import java.io.*;
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        Queue<Integer> queue = new LinkedList<>();
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] input = br.readLine().split(" ");
-        int N = Integer.parseInt(input[0]);
-        int K = Integer.parseInt(input[1]);
-        for(int i = 1; i <= N; i++) queue.offer(i);
-        StringBuilder sb = new StringBuilder();
-        sb.append("<");
-        int num = 0;
-        while(!queue.isEmpty()){
-            num++;
-            if(num % K == 0){
-                int number = queue.poll();
-                if(queue.size() == 0) sb.append(number);
-                else sb.append(number).append(", ");
-            }
-            else queue.offer(queue.poll());
-        }
-        sb.append(">");
-
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        bw.write(sb.toString());
-        bw.flush();
-
-        br.close();
-        bw.close();
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		String[] input = br.readLine().split(" ");
+		int N = Integer.parseInt(input[0]);
+		int K = Integer.parseInt(input[1]);
+		
+		List<Integer> list = new ArrayList<>();
+		for(int i = 1; i <= N; i++) list.add(i);
+		
+		bw.write("<");
+		int index = 0; 
+		while(!list.isEmpty()) {
+			index = (index + K -1) % list.size();
+			int tmp = list.remove(index);
+			if(list.size() >= 1) bw.write(tmp + ", ");
+			else bw.write(String.valueOf(tmp));
+		}
+		
+		bw.write(">");
+		
+		bw.flush();
+		bw.close();
+		br.close();
+		
+		
+	}
 }
+
