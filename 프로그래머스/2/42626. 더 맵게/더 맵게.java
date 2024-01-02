24ec95c7594cbe371867d135513e647628aa4795
Jaeung <75228991+jaewoong-gwon@users.noreply.github.com>
2024-01-02 17:38:06 +0900

    [level 2] Title: 더 맵게, Time: 1567.83 ms, Memory: 123 MB -BaekjoonHub

---
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -0,0 +1,44 @@
+import java.util.*;
+class Solution {
+    static PriorityQueue<Integer> queue;
+    public int solution(int[] scoville, int K) {
+        int answer = 0;
+        
+        queue = new PriorityQueue<>((o1,o2)->{
+            return o1.compareTo(o2);
+        });
+        
+        for(int i : scoville) queue.add(i);
+         
+        int sum = 0;
+        int min = queue.peek();
+        while(K > min && queue.size() > 1){
+            // if(queue.size() < 2) break; -> 하나만 남았을때 스코빌 지수를 넘는다면 틀림.
+                int first = queue.poll();
+                int second = queue.poll();
+
+                sum = first + (2 * second);
+                queue.add(sum);
+                answer++;
+                min = queue.peek();
+                //print();
+                //if(check(K)) return answer;
+                
+        }
+        
+        if(min < K) return -1;
+        
+        return answer;
+    }
+    
+    static void print(){
+        List<Integer> tmp = new ArrayList<>();
+        while(!queue.isEmpty()){
+            int i = queue.poll();
+            tmp.add(i);
+            System.out.println(i);
+        }
+        System.out.println("------------------");
+        for(int i : tmp) queue.add(i);
+    }
+}
